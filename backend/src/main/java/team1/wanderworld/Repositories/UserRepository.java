247095eedcc71812f9d50694dbe5546f3a10526b
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-11-06 21:28:57 +0900

    user 필드명 수정

--- backend/src/main/java/team1/wanderworld/Repositories/UserRepository.java
+++ backend/src/main/java/team1/wanderworld/Repositories/UserRepository.java
@@ -7,7 +7,7 @@ import team1.wanderworld.Models.User;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User,Long> {
-    Optional<User> findByNickname(String nickName);
+    Optional<User> findByUsername(String userName);
     Optional<User> findById(String id);
 }
 
