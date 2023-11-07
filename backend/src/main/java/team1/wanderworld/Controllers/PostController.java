15e868963b17264c65556786bde5d035ed5f006f
BuildTools <unconfigured@null.spigotmc.org>
2023-11-07 20:26:55 +0900

    컨트롤러 수정

--- backend/src/main/java/team1/wanderworld/Controllers/PostController.java
+++ backend/src/main/java/team1/wanderworld/Controllers/PostController.java
@@ -27,47 +27,18 @@ public class PostController {
         return new ResponseEntity<>(postMapper.postToPostResponseDto(createdPost), HttpStatus.CREATED);
     }
 
-
     // 포스트 수정
     @PutMapping("/{post-id}/edit")
     public ResponseEntity putPost(@PathVariable("post-id") String postId,
-                                  @Valid @RequestBody PostDto.PutDto requestBody
-//                                    ,Authentication authentication
-    ){
-
-//        Map<String,Object> principal = (Map) authentication.getPrincipal();
-//        long jwtPostId = ((Number) principal.get("postId")).longValue();
-//
-//        if(jwtPostId != (postId)){
-//            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-//        }
+                                  @Valid @RequestBody PostDto.PutDto requestBody){
         requestBody.setId(postId);
         Post updatedPost = postService.updatePost(postMapper.postPutDtoToPost(requestBody));
         return new ResponseEntity<>(postMapper.postToPostResponseDto(updatedPost), HttpStatus.OK);
     }
 
-
-    // [회원프로필 조회]
-    @GetMapping("/{post-id}")
-    public ResponseEntity getPost(@PathVariable("post-id") String postId){
-        Post foundPost = postService.findPost(postId);
-        return new ResponseEntity<>(postMapper.postToPostResponseDto(foundPost), HttpStatus.OK);
-    }
-
-
-
-    // [회원탈퇴]
+    // 포스트 삭제
     @DeleteMapping("/{post-id}")
-    public ResponseEntity deletePost(@PathVariable("post-id") String postId
-//            ,Authentication authentication
-    ){
-
-//        Map<String,Object> principal = (Map) authentication.getPrincipal();
-//        long jwtPostId = ((Number) principal.get("postId")).longValue();
-//
-//        if(jwtPostId != postId){
-//            return new ResponseEntity(HttpStatus.FORBIDDEN);
-//        }
+    public ResponseEntity deletePost(@PathVariable("post-id") String postId){
         postService.deletePost(postId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
