f8cdc0139a6b9cfe2d47b057901fe321510b1c68
이채은 <76944115+ChaeEunLee00@users.noreply.github.com>
2023-12-02 01:21:16 +0900

    image api 구현

---
+++ backend/src/main/java/team1/wanderworld/image/S3UploadController.java
@@ -0,0 +1,43 @@
+package team1.wanderworld.image;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/upload")
+@RequiredArgsConstructor
+public class S3UploadController {
+
+    private final AmazonS3 amazonS3;
+    //private final S3Uploader s3Uploader;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostMapping
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName=file.getOriginalFilename();
+            ObjectMetadata metadata= new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3.putObject(bucket,fileName,file.getInputStream(),metadata);
+            return ResponseEntity.ok(amazonS3.getUrl(bucket, fileName).toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
