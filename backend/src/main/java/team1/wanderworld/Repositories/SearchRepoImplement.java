1b41073eeaea250238cc056658bafec2f328a54b
Funda30 <107970477+Funda30@users.noreply.github.com>
2023-12-02 21:56:40 +0900

    search implement

---
+++ backend/src/main/java/team1/wanderworld/Repositories/SearchRepoImplement.java
@@ -0,0 +1,58 @@
+package team1.wanderworld.Repositories;
+
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.stereotype.Component;
+import team1.wanderworld.Models.Post;
+import team1.wanderworld.Models.User;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@Component
+public class SearchRepoImplement implements SearchRepository{
+
+    @Autowired
+    public MongoClient client;
+
+    @Autowired
+    public MongoConverter converter;
+    @Override
+    public List<Post> FindInPosts(String text) {
+        final List<Post> posts = new ArrayList<>();
+        MongoDatabase database = client.getDatabase("WanderWorld");
+
+        MongoCollection<Document> collection = database.getCollection("post");
+
+        AggregateIterable<Document> result = collection.aggregate(Arrays.asList(new Document("$search",
+                        new Document("text",
+                                new Document("query", text)
+                                        .append("path", Arrays.asList("content", "hashtags")))),
+                new Document("$limit", 5L)));
+
+        result.forEach(doc -> posts.add(converter.read(Post.class, doc)));
+        return posts;
+    }
+
+    @Override
+    public List<User> FindInUsers(String text) {
+        final List<User> users = new ArrayList<>();
+        MongoDatabase database = client.getDatabase("WanderWorld");
+
+        MongoCollection<Document> collection = database.getCollection("user");
+
+        AggregateIterable<Document> result = collection.aggregate(Arrays.asList(new Document("$search",
+                        new Document("text",
+                                new Document("query", text)
+                                        .append("path", Arrays.asList("username")))),
+                new Document("$limit", 5L)));
+
+        result.forEach(doc -> users.add(converter.read(User.class, doc)));
+        return users;
+    }
+}
