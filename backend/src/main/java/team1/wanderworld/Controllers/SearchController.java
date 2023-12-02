162af8523af7737cef319a0d55b9af10ea848a0e
Funda30 <107970477+Funda30@users.noreply.github.com>
2023-12-02 21:55:45 +0900

    search controller

--- backend/src/main/java/team1/wanderworld/Controllers/SearchController.java
+++ backend/src/main/java/team1/wanderworld/Controllers/SearchController.java
@@ -3,37 +3,29 @@ package team1.wanderworld.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team1.wanderworld.Models.Post;
 import team1.wanderworld.Models.User;
-import team1.wanderworld.Services.SearchService;
+import team1.wanderworld.Repositories.SearchRepository;
 
 import java.util.List;
-
 @RestController
-@RequestMapping("/search")
+@CrossOrigin(origins = "http://localhost:8080")
 public class SearchController {
-
     @Autowired
-    private SearchService searchService;
+    SearchRepository searchRepo;
 
-    @GetMapping
-    public ResponseEntity<List<Post>> search(@RequestParam String keyword) {
-        List<Post> searchResults = searchService.searchPosts(keyword);
-        return new ResponseEntity<>(searchResults, HttpStatus.OK);
+    @GetMapping("/search/posts/{text}")
+    @CrossOrigin
+    public List<Post> searchPosts(@PathVariable("text")  String text)
+    {
+        return searchRepo.FindInPosts(text);
     }
-
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> searchUsers(@RequestParam String keyword) {
-        List<User> searchResults = searchService.searchUsers(keyword);
-
-        if(!searchResults.isEmpty()) {
-            return new ResponseEntity<>(searchResults, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(searchResults, HttpStatus.NOT_FOUND);
-        }
+    @GetMapping("/search/users/{text}")
+    @CrossOrigin
+    public List<User> searchUsers(@PathVariable("text")  String text)
+    {
+        return searchRepo.FindInUsers(text);
     }
 }
+
